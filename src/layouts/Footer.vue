c6e88e1fe200b8f04797ed0f65da291ab1c2402a
Denys Malieiev <denysmalieiev@gmail.com>
2023-08-08 13:16:47 +0500

    add footer to MainLayout

---
+++ src/layouts/Footer.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { onMounted } from "vue";
+
+onMounted(()=>{
+  console.log("Footer mounted")
+})
+</script>
+
+<template>
+  <footer>
+    <h2>Footer</h2>
+  </footer>
+</template>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { onMounted } from "vue";
 import Header from "./Header.vue";
+import Footer from "./Footer.vue";
 
 onMounted(()=>{
   console.log("Main layout mounted")
@@ -13,6 +14,7 @@ onMounted(()=>{
   <main class="main">
     <slot/>
   </main>
+  <Footer/>
 </div>
 </template>
 
