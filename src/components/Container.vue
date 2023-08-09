96e5978ed1c5aad2344a3df026d41b1228347896
Denys Malieiev <denysmalieiev@gmail.com>
2023-08-09 10:15:48 +0500

    add Container com

---
+++ src/components/Container.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { onMounted } from "vue";
+
+onMounted(()=>{
+  console.log("container com mounted")
+})
+</script>
+
+<template>
+  <div class="container">
+    <slot/>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.container{
+  max-width: 1230px;
+  margin: 0 auto;
+  width: 100%;
+  padding-left: 15px;
+  padding-right: 15px;
+}
+</style>
