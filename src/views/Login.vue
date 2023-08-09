a6d59256781d072564ff7405c22e5e44cffab8a7
Denys Malieiev <denysmalieiev@gmail.com>
2023-08-09 10:16:05 +0500

    add Login page

---
+++ src/views/Login.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { onMounted } from "vue";
+import Container from "../components/Container.vue";
+
+console.log("login page",Date.now())
+
+onMounted(()=>{
+  console.log("login page mounted",Date.now())
+})
+</script>
+
+<template>
+  <div>
+    <Container>
+      <h2>Login page</h2>
+    </Container>
+  </div>
+</template>
+
+<style scoped lang="scss">
+</style>
