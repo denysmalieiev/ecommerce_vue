132c7fd2b209425ac26c0ec5664577bfb7ac4cf6
Denys Malieiev <denysmalieiev@gmail.com>
2023-08-10 13:32:09 +0500

    added Popup component

---
+++ src/components/Modal.vue
@@ -0,0 +1,45 @@
+<script setup>
+const props=defineProps({
+  show:Boolean,
+})
+console.log("props",props.show)
+</script>
+
+<template>
+  <Teleport to="body">
+    <div v-if="show" class="modal">
+      <div class="content">
+         <slot name="header">
+          <span>This is a header block</span>
+         </slot>
+         <slot name="body">
+          <span>This is a header block</span>
+         </slot>
+         <slot name="footer">
+          <span>This is a footer block</span>
+         </slot>
+        <button @click="$emit('close')">Close</button>
+      </div>
+    </div>
+  </Teleport>
+</template>
+
+<style scoped lang="scss">
+.modal{
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background-color: rgba(0,0,0,.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.content{
+  width: 100%;
+  max-width: 500px;
+  height: 200px;
+  background-color: red;
+}
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,18 +1,37 @@
 <script setup>
-import {ref,onMounted} from "vue";
+import { ref } from "vue";
 import Container from "../components/Container.vue";
-const a=ref('abc')
+import Modal from "../components/Modal.vue";
 
-onMounted(()=>{
-  console.log("About View mounted")
-})
+const showModal=ref(false)
+const close=()=>showModal.value=false;
+const open=()=>showModal.value=true;
 </script>
 
 <template>
   <div>
     <Container>
-      <p>{{ a }}</p>
       <h2>About View</h2>
+      <button class="button" @click="open">Open Modal</button>
+      <modal :show="showModal" @close="close">
+       <h3> This is a modal</h3>
+       <template #header>
+        <h2>This is a header</h2>
+       </template>
+       <template #body>
+        <h2>This is a body</h2>
+       </template>
+      </modal>
     </Container>
   </div>
 </template>
+
+<style scoped lang="scss">
+h2{
+  margin-bottom: 40px;
+}
+.button{
+  padding:20px;
+  font-size: 18px;
+}
+</style>
