c02013789dc1e61b2a3723964d8a9f9a439bdb3b
Denys Malieiev <denysmalieiev@gmail.com>
2023-08-11 10:51:59 +0500

    minor changes

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,27 +1,11 @@
 <script setup>
-import { ref } from "vue";
-import Container from "../components/Container.vue";
-import Modal from "../components/Modal.vue";
-
-const showModal=ref(false)
-const close=()=>showModal.value=false;
-const open=()=>showModal.value=true;
+  import Container from "../components/Container.vue";
 </script>
 
 <template>
   <div>
     <Container>
       <h2>About View</h2>
-      <button class="button" @click="open">Open Modal</button>
-      <modal :show="showModal" @close="close">
-       <h3> This is a modal</h3>
-       <template #header>
-        <h2>This is a header</h2>
-       </template>
-       <template #body>
-        <h2>This is a body</h2>
-       </template>
-      </modal>
     </Container>
   </div>
 </template>
@@ -29,9 +13,5 @@ const open=()=>showModal.value=true;
 <style scoped lang="scss">
 h2{
   margin-bottom: 40px;
-}
-.button{
-  padding:20px;
-  font-size: 18px;
 }
 </style>
--- src/views/UiView.vue
+++ src/views/UiView.vue
@@ -3,22 +3,42 @@ import { ref } from "vue";
 
 import Container from '../components/Container.vue';
 import SimpleDrawer from '../components/SimpleDrawer.vue';
+import Modal from "../components/Modal.vue";
 
-const show=ref(false);
 
-const open=()=>show.value=true;
-const close=()=>show.value=false;
+const showDrawer=ref(false);
+
+const openDrawer=()=>showDrawer.value=true;
+const closeDrawer=()=>showDrawer.value=false;
+
+const showModal=ref(false)
+const closeModal=()=>showModal.value=false;
+const openModal=()=>showModal.value=true;
 </script>
 
 <template>
     <Container class="ui">
-       <section>
-          <simple-drawer :show="show" @close="close" >
-           <div>
+       <section class="section">
+        <h2>Simple Drawer</h2>
+          <simple-drawer :show="showDrawer" @close="closeDrawer" >
+           <h2>
               About
-            </div>
+            </h2>
           </simple-drawer>
-          <button @click="open" class="btn">Open drawer</button>
+          <button @click="openDrawer" class="btn">Open drawer</button>
+       </section>
+       <section class="section">
+        <h2>Simple Popup</h2>
+        <button class="btn" @click="openModal">Open Modal</button>
+      <modal :show="showModal" @close="closeModal">
+       <h3> This is a modal</h3>
+       <template #header>
+        <h2>This is a header</h2>
+       </template>
+       <template #body>
+        <h2>This is a body</h2>
+       </template>
+      </modal>
        </section>
     </Container>
 </template>
@@ -28,10 +48,16 @@ const close=()=>show.value=false;
   padding-top: 80px;
   padding-bottom: 80px;
 }
-section{
+
+.section{
+  padding-top: 20px;
   padding-bottom: 80px;
+  border-bottom: 1px solid darkcyan;
 }
 
+h2{
+  margin-bottom: 20px;
+}
 .btn{
   padding: 8px 15px;
   font-size: 18px;
