0833fd1b111ab1ae353cfc32a4f4db472c2ed6b6
Denys Malieiev <denysmalieiev@gmail.com>
2023-08-16 18:08:59 +0500

    resolved confilict

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <script setup>
-<<<<<<< HEAD
 import MainLayout from "./layouts/MainLayout.vue";
 import { useCounterStore } from './stores/counter'
 
@@ -10,9 +9,6 @@ counter.count++
 counter.$patch({ count: counter.count + 1 })
 // or using an action instead
 counter.increment()
-=======
-import MainLayout from './layouts/MainLayout.vue';
->>>>>>> 93f7936bd5677c9553ed5690bff2b24f5f2df73c
 </script>
 
 <template>
