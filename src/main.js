2d548cdf573242adf4bbd4ec1177dc6e16124068
Denys Malieiev <denysmalieiev@gmail.com>
2023-08-17 11:06:54 +0500

    rename style.css to style.scss

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
 
-import './style.css';
+import './style.scss';
 
 const pinia = createPinia();
 const app = createApp(App);
